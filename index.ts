3bd74eb966d27a61a7d98858f25ef2858995c600
JaeHo Song <thruthesky@gmail.com>
2025-02-26 16:21:13 +0800

    import first work

---
+++ index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from 'express';
+const app = express()
+
+const serverName: string = 'Hello World';
+
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: ['index.html'],
+    maxAge: '1d',
+    redirect: false,
+    setHeaders(res: Response) {
+        res.set('x-timestamp', 'Date.now: ' + Date.now())
+    }
+}
+
+app.use(express.static('public', options))
+
+app.get('/add', (req: Request, res: Response) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const a = parseInt(url.searchParams.get('a') || '0');
+    const b = parseInt(url.searchParams.get('b') || '0');
+    res.json({ message: 'Addition', result: a + b })
+});
+
+
+app.listen(13005, () => {
+    console.log(`Server [${serverName}] is running on port 13005 ....`)
+}).on('error', (err) => {
+    console.error(err);
+});
+
+
+
+
+
+
+
